d7f7293788b7ed9a73083f9581b19185fce19534
bhktisagar52-ux <bhktisagar52@gmail.com>
2026-01-05 00:16:29 +0530

    Update server.js

--- genz2/server.js
+++ genz2/server.js
@@ -1,16 +1,24 @@
+const express = require('express')
 const { createServer } = require('http')
-const { parse } = require('url')
 const next = require('next')
+const path = require('path')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  const httpServer = createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
+  const server = express()
+
+  // Serve static files from .next/static
+  server.use('/_next/static', express.static(path.join(__dirname, '.next/static')))
+
+  // Handle all other requests with Next.js
+  server.all('*', (req, res) => {
+    return handle(req, res)
   })
+
+  const httpServer = createServer(server)
 
   // Initialize WebSocket server
   const { initWebSocket } = require('./src/lib/websocket')
